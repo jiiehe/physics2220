aef66da44f4b1386298a4abd877275970c1a2fee
Jin He <u0870268@ad.utah.edu>
2018-06-04 14:20:33 -0600

    need to figure out question 2 and 3

---
+++ calculation/calculation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //question 1
+            kq1q2rcalculation();
+        }
+        public static void kq1q2rcalculation()
+        {
+            double k = 8.99 * Math.Pow(10, 9);
+            double r = Math.Pow(0.300, 2);
+            double q1 = 12 * Math.Pow(10, -9);
+            double q2 = 13 * Math.Pow(10, -9);
+            double result = (k * q1 * q2) / r;
+            double q = (q1 + q2) / 2;
+            double result2 = (k * Math.Pow(q, 2)) / r;
+            Console.WriteLine(result2);
+            Console.Read();
+        }
+    }
+
+   
+    
+
+
+}
