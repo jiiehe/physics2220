87e3c005b804befaee8509e07d677672bb63a312
jiiehe <1173311048@qq.com>
2018-06-05 06:08:49 +0800

    add two more questions,

--- calculation/calculation/Program.cs
+++ calculation/calculation/Program.cs
@@ -11,7 +11,8 @@ namespace calculation
         static void Main(string[] args)
         {
             //question 1
-            kq1q2rcalculation();
+            //twopikqcalculation();
+            question6();
         }
         public static void kq1q2rcalculation()
         {
@@ -25,7 +26,33 @@ namespace calculation
             Console.WriteLine(result2);
             Console.Read();
         }
+        public static void twopikqcalculation()
+        {
+            double k = 8.99 * Math.Pow(10, 9);
+            double twopi = 2 * 3.14;
+            double q = 7.5 * Math.Pow(10, -6);
+            double Lpower2 = Math.Pow(15 * Math.Pow(10, -2),2);
+            double result = (twopi * k * q) / Lpower2;
+            Console.WriteLine(result);
+            Console.Read();
+        }
+        public static void question6()
+        {
+            double vpower2 = Math.Pow(17.3, 2);
+            double s2 = 3.5*2;
+            double a = (vpower2 - 0) / s2;
+            Console.WriteLine(a);
+            double m = 1*Math.Pow(10, -3);
+            double g = 9.8;
+            double e = 1 * Math.Pow(10, 4);
+            double result = (m * a - m * g) / e;
+            Console.WriteLine(result);
+            Console.Read();
+        }
+
+
     }
+ 
 
    
     
