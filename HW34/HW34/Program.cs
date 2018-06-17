7724de29504fb4bd167da0114e2abda7261fe443
jiiehe <1173311048@qq.com>
2018-06-17 12:32:39 +0800

    add name for it

--- HW34/HW34/Program.cs
+++ HW34/HW34/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/// <summary>
+/// Name: Jin HE, uid: u0870268
+/// </summary>
 namespace HW34
 {
     class Program
